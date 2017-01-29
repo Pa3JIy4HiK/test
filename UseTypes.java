f28ee22a2913cfc9c6680f9123203d90d951eed1
Pa3JIy4HiK <paradoks6666@gmail.com>
2017-01-29 19:41:27 +0300

    TEST

---
+++ UseTypes.java
@@ -0,0 +1,11 @@
+class UseOperators {
+    public static void main(String[] args) {
+        double integer1 = 190 ;
+        double integer2 = 10;
+
+        double doubleValue = (double) integer2 / integer1;
+
+
+        System.out.println(doubleValue);
+    }
+}
