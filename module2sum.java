f91e3ea797fb884ed36e0976feaa66cf2b33ed9c
Pa3JIy4HiK <paradoks6666@gmail.com>
2017-02-01 11:07:34 +0300

    TEST!

---
+++ module2sum.java
@@ -0,0 +1,22 @@
+public class module2sum {
+    public static void main(String[] args) {
+
+        int[] arrayOffinger = new int[10];
+        double[] arrayOfarms = new double[2];
+
+
+        arrayOffinger[0] = 1;
+        arrayOffinger[1] = 2;
+        arrayOffinger[2] = 3;
+        arrayOffinger[3] = 4;
+        arrayOffinger[4] = 5;
+        arrayOffinger[5] = 6;
+        arrayOffinger[6] = 7;
+        arrayOffinger[7] = 8;
+        arrayOffinger[8] = 9;
+        arrayOffinger[9] = 10;
+
+        for (int i = 0; i < arrayOffinger.length; i++) System.out.println(("breaking fingers" + arrayOffinger[i]) );
+
+    }
+}
